d73e0c7543d945c4a05d1af696ba0d058b04b299
任毅 <191250116@smail.nju.edu.cn>
2021-01-24 21:16:46 +0800

    1

---
+++ code/spider/news/lizhiSpider.py
@@ -0,0 +1,82 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import random
+
+
+def get_content(str):
+    ans = ""
+    write = False
+    for i in str:
+        if i == '>':
+            write = True
+        elif i == '<':
+            write = False
+        else:
+            if write:
+                ans += i
+    return ans
+
+
+PROXY_POOL_URL = 'http://localhost:5555/random'
+
+
+def get_proxy():
+    try:
+        response = requests.get(PROXY_POOL_URL)
+        if response.status_code == 200:
+            return response.text
+    except ConnectionError:
+        return None
+
+
+base_url = 'https://so.jstv.com/?keyword=%E7%96%AB%E6%83%85&page='
+head = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36'
+}
+
+
+filename = 'lizhinews.txt'
+start_page = 19
+end_page = 3715
+error_count = 0
+
+for i in range(start_page, end_page + 1):
+    real_url = base_url + str(i)
+    r = requests.get(real_url, headers=head)
+    r.raise_for_status()
+    r.encoding = r.apparent_encoding
+
+    soup = BeautifulSoup(r.content, 'html.parser')
+    list = soup.find('div', class_="lzxw_lxz").find('ul').find_all('li')
+
+    for element in list:
+        try:
+            title = str(element.find('span').find('a').string)
+            href = str(element.find('span').find('a')['href'])
+
+            news_r = requests.get(href, headers=head)
+            news_soup = BeautifulSoup(news_r.content, 'html.parser')
+
+            time1 = str(news_soup.find('p', class_='info fL').find('span', class_='time').string)
+            source = str(news_soup.find('p', class_='info fL').find('span', class_='source').string)
+
+            text = str(news_soup.find('div', class_='content').find_all('p', style=""))
+            content = get_content(text)
+
+            temp = title + '\n' + href + "\n" + time1 + "  " + source + '\n' + content + '\n' + '\n'
+            f = open(filename, 'a', encoding='utf-8')
+            f.write(temp)
+        except Exception:
+            error_count += 1
+            continue
+
+
+    print("爬取第" + str(i) + "页已完成")
+    time.sleep(random.random())
+
+print("爬取" + str(start_page) + "-" + str(end_page) + "页完成")
+print("共产生" + str(error_count) + "处错误")
+
+
+
---
+++ code/spider/weibo/weiboSpider.py
@@ -0,0 +1,95 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import random
+
+
+def get_content(str):
+    ans = ""
+    write = False
+    count = 0
+    for i in str:
+        if i == '>':
+            write = True
+        elif i == '<':
+            write = False
+        elif i == '#':
+            count += 1
+        else:
+            if write and (count % 2 == 0):
+                ans += i
+    return ans
+
+
+PROXY_POOL_URL = 'http://localhost:5555/random'
+
+
+def get_proxy():
+    try:
+        response = requests.get(PROXY_POOL_URL)
+        if response.status_code == 200:
+            return response.text
+    except ConnectionError:
+        return None
+
+
+base_url = 'https://weibo.cn/search/mblog?hideSearchFrame=&keyword=%E7%96%AB%E6%83%85&advancedfilter=1&hasori=1&starttime=20200310&endtime=20200630&sort=time&page='
+head = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'cookie': '_T_WM=71345515258; SCF=Aqde6NoTDhsEfl8BJwQI0Z5vgtEA7CtnP64n-cbOfSrhZGWfRsM1x_sFlXR-UOz-5RW39QwcA9Ej_8s6wbVnTLg.; SUB=_2A25NCGUqDeRhGeBP6VEQ9CrIzDqIHXVu8wtirDV6PUJbktANLUzakW1NRX8UHFN3Jey8SxqcHqtLDGi4HmQte34B; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WhseaBECl4pMTT9KQMmhDS95NHD95QceKz0eKBXShMcWs4DqcjsMrSr9NSr',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'
+}
+
+filename = 'weibo.txt'
+start_page = 1
+end_page = 267
+error_count = 0
+
+for i in range(start_page, end_page):
+    real_url = base_url + str(i)
+    r = requests.get(real_url, headers=head, proxies=get_proxy())
+    r.raise_for_status()
+    r.encoding = r.apparent_encoding
+
+    soup = BeautifulSoup(r.content, 'html.parser')
+    list = soup.find_all('div', class_='c')
+
+
+    for j in list:
+        try:
+            # print(j.find('div').find('a')['href'])
+            # print(j.find('div').find('a').string)
+            # print(get_content(str(j.find('div'))))
+            # print(j.find_all('div')[-1].find_all('a')[-4].string)
+            # print(j.find_all('div')[-1].find_all('a')[-3].string)
+            # print(j.find_all('div')[-1].find_all('a')[-2].string)
+            # print(j.find_all('div')[-1].find('span', class_='ct').string)
+
+
+            user_name = str(j.find('div').find('a').string)
+            user_http = str(j.find('div').find('a')['href'])
+            content = get_content(str(j.find('div')))
+            zan = str(j.find_all('div')[-1].find_all('a')[-4].string[-2])
+            zhuan = str(j.find_all('div')[-1].find_all('a')[-3].string[-2])
+            ping = str(j.find_all('div')[-1].find_all('a')[-2].string[-2])
+            other = str(j.find_all('div')[-1].find('span', class_='ct').string).split(" ")
+            time1 = other[0] + " " + other[1]
+            source = other[2]
+
+            open(filename, 'a').write("a")
+            open(filename, 'a').write(user_name + '\n' + user_http + '\n' + content + '\n' + zan + '\n' + zhuan + '\n' + ping + '\n' + time1 + '\n' + source + '\n' + '\n')
+
+        except Exception:
+            error_count += 1
+            continue
+
+    time.sleep(random.random())
+    print("爬取第" + str(i) + "页已完成")
+
+print("爬取" + str(start_page) + "-" + str(end_page) + '页已完成')
+print("共产生" + str(error_count) + "处错误")
+
+
+
+
+
